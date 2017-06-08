8b974e7c06b90675ccdb4efff23f4846c1040b38
evrmlya <evermilyea@evolytics.com>
2017-06-07 22:55:05 -0500

    add extsort problem

---
+++ practice-book/working-with-data/strings.py
@@ -0,0 +1,29 @@
+print 'el' in 'hello'
+print 'EL' in 'hello'
+
+def includes(include, string, transform=None):
+    if transform != None:
+        return transform(include) in transform(string)
+    return include in string
+
+print includes('EL', 'hello', transform=lambda s: s.lower())
+
+print ' hello world\n'.strip()
+
+a = 'hello'
+b = 'python'
+print "%s %s" % (a, b)
+
+# Problem 16
+# Write a function `extsort` to sort a list
+# of files based on extension.
+
+def split_file(f):
+    split = f.split('.')
+    return split[1] + '.' + split[0]
+
+def extsort(files):
+    return sorted(files, key=split_file)
+
+print extsort(['a.c', 'a.py', 'b.py', 'appy.py', 
+    'bar.txt', 'foo.txt', 'txty.txt', 'x.c'])
