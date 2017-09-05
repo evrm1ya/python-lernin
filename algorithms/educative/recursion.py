d30371f1c6e4c1f3f49b1c4f15db6a35250c06c5
evrmlya <evermilyea@evolytics.com>
2017-09-05 18:56:08 -0500

    add rest of recursion problems

--- algorithms/educative/recursion.py
+++ algorithms/educative/recursion.py
@@ -11,8 +11,46 @@ def factorial(n):
         return 1
     return n * factorial(n - 1)
 
+def is_palindrome(word):
+    if word == '' or len(word) == 1:
+        return True
+
+    if word[0] != word[-1]:
+        return False
+
+    return is_palindrome(word[1:-1])
+
+
+def is_odd(x):
+    return x % 2 != 0
+
+def power(x, n):
+    if n == 0:
+        return 1
+
+    if n < 0:
+        return 1 / power(x, n * -1)
+
+    if is_odd(n):
+        return x * power(x, n - 1)
+
+    y = power(x, n / 2)
+    return y * y
+
+
 if __name__ == '__main__':
     print(factorial_iterative(5))
     print(factorial(0))
     print(factorial(5))
-    print(factorial(200))
+    #print(factorial(200))
+
+    print('rotor is palindrome', is_palindrome('rotor'))
+    print('motor is palindrome', is_palindrome('motor'))
+
+    print(power(2.0, 0))
+    print(power(3.0, 2))
+    print(power(5.0, 3))
+    print(power(2.0, -2))
+    print(power(5.0, -3))
+    print(power(2.0, 4))
+    print(power(2.0, -4))
